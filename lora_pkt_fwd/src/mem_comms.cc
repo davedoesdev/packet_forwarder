1e582d8dce1bc6c70be223ec6853c1135976c104
David Halls <dahalls@gmail.com>
2018-01-28 08:03:38 +0000

    Move to std::chrono::duration

--- lora_pkt_fwd/src/mem_comms.cc
+++ lora_pkt_fwd/src/mem_comms.cc
@@ -1,29 +1,36 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <errno.h>
 #include <queue>
 #include <vector>
+#include <chrono>
 
 #define SOCKET_UP   0
 #define SOCKET_DOWN 1
 
+using namespace std::chrono_literals;
+
 class Queue
 {
 public:
-    ssize_t send(const void *buf, size_t len,
-                 size_t hwm, const struct timeval &timeout)
+    ssize_t send(const void * /*buf*/, size_t /*len*/,
+                 ssize_t /*hwm*/, const std::chrono::microseconds &/*timeout*/)
     {
-        // timeout 0 means block
-        // hwm 0 means write always
-        // otherwise, wait until buffered data < hwm
+        // timeout < 0 means block
+        // hwm < 0 means write always
+        // otherwise, wait until buffered data <= hwm
+        return 0;
     }
 
-    ssize_t recv(void *buf, size_t len, const struct timeval &timeout)
+    ssize_t recv(void * /*buf*/, size_t /*len*/,
+                 const std::chrono::microseconds &/*timeout*/)
     {
-        // timeout 0 means block
+        // timeout < 0 means block
+        return 0;
     }
 
 private:
@@ -34,18 +41,31 @@ private:
 class Link
 {
 public:
+    Link()
+    {
+        reset();
+    }
+
     void reset()
     {
-        timeout = {};
+        timeout = -1us;
     }
 
     void set_timeout(const struct timeval &timeout)
     {
-        this->timeout = timeout;
+        if (timerisset(&timeout))
+        {
+            this->timeout = timeout.tv_sec * 1s + timeout.tv_usec * 1us;
+        }
+        else
+        {
+            // setsockopt uses 0 to block
+            this->timeout = -1us;
+        }
     }
 
 private:
-    struct timeval timeout;
+    std::chrono::microseconds timeout;
     Queue from_fwd, to_fwd;
 };
 
